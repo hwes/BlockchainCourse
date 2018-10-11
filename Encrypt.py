5e71ec7958fe4867a28de9a2f737278c64e4fcb2
Harald Wesenberg <haraldwesenberg@Haralds-Macbook.local>
2018-10-11 15:48:09 +0200

    Added chr()

--- Encrypt.py
+++ Encrypt.py
@@ -6,6 +6,7 @@ encryptedText = clearText ^ encryptionKey
 newClearText = encryptedText ^ encryptionKey
 
 print(str(clearText) + '/' + str(encryptionKey) + '/' + str(encryptedText) + '/' + str(newClearText))
+print(chr(clearText) + '/' + chr(encryptionKey) + '/' + chr(encryptedText) + '/' + chr(newClearText))
 print(bin(clearText) + '/' + bin(encryptionKey) + '/' + bin(encryptedText) + '/' + bin(newClearText))
 # print(clearText + '/' + encryptionKey + '/' + encryptedText)
 
